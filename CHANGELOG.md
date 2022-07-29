d1a572ff3e9ae88a7e16620ad5aee611d0e33159
Campbell He <kp.campbell.he@duskmoon314.com>
2022-07-29 12:58:56 +0800

    feat: add clap for argparse

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,5 +9,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- Use `clap` to parse arg
 - `HTTP Methods` of httpbin
 - `Request inspection` of httpbin
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use std::net::{IpAddr, Ipv4Addr};
+
+use clap::Parser;
 use poem::{listener::TcpListener, Route, Server};
 use poem_openapi::{OpenApiService, Tags};
 
 pub(crate) mod http_methods;
 pub(crate) mod request_inspection;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    /// The address to listen on.
+    #[clap(long, value_parser)]
+    address: Option<String>,
+
+    /// The ip to listen on.
+    #[clap(long, value_parser, default_value_t = IpAddr::V4(Ipv4Addr::LOCALHOST))]
+    ip: IpAddr,
+
+    /// The port to listen on.
+    #[clap(long, value_parser, default_value = "8000")]
+    port: u16,
+}
 
 #[derive(Tags)]
 enum ApiTags {
@@ -17,6 +36,8 @@ enum ApiTags {
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let args = Args::parse();
+
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "poem=debug");
     }
@@ -28,13 +49,18 @@ async fn main() -> Result<(), std::io::Error> {
         env!("CARGO_PKG_VERSION"),
     )
     .description(env!("CARGO_PKG_DESCRIPTION"))
-    .server("http://127.0.0.1:3000/");
-    // let ui = api_service.rapidoc();
+    .license(env!("CARGO_PKG_LICENSE"))
+    .server(if args.address.is_some() {
+        args.address.unwrap()
+    } else {
+        format!("http://{}:{}", args.ip, args.port)
+    });
+
     let swagger = api_service.swagger_ui();
     let rapidoc = api_service.rapidoc();
     let redoc = api_service.redoc();
 
-    Server::new(TcpListener::bind("127.0.0.1:3000"))
+    Server::new(TcpListener::bind(format!("{}:{}", args.ip, args.port)))
         .run(
             Route::new()
                 .nest("/", api_service)
