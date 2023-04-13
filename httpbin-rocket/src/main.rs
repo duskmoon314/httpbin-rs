b7a2f11029edbbd71f23fb0a8e83796f2e92d630
Campbell He <kp.campbell.he@duskmoon314.com>
2023-04-13 09:44:00 +0800

    feat(rocket): set cors to mirror request

--- httpbin-rocket/src/main.rs
+++ httpbin-rocket/src/main.rs
@@ -1,12 +1,13 @@
+use anyhow::Result;
+use httpbin::cli::Cli;
+use rocket::{fairing::AdHoc, Config};
+
 mod data;
 mod http_method;
 mod request_inspection;
 
-use httpbin::cli::Cli;
-use rocket::{fairing::AdHoc, Config};
-
-#[rocket::launch]
-fn rocket() -> _ {
+#[rocket::main]
+async fn main() -> Result<()> {
     let cfg = Cli::parse().load_config();
 
     let rocket_config = Config {
@@ -15,11 +16,20 @@ fn rocket() -> _ {
         ..Config::default()
     };
 
-    rocket::custom(rocket_config)
+    let cors = rocket_cors::CorsOptions::default()
+        .allowed_origins(rocket_cors::AllowedOrigins::all())
+        .to_cors()?;
+
+    let _ = rocket::custom(rocket_config)
+        .attach(cors)
         .attach(AdHoc::on_ignite("mount_data", data::api))
         .attach(AdHoc::on_ignite("mount_http_method", http_method::api))
         .attach(AdHoc::on_ignite(
             "mount_request_inspection",
             request_inspection::api,
         ))
+        .launch()
+        .await?;
+
+    Ok(())
 }
